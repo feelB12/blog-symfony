feccae9ae19aa793e40a4a08b36f4c3616457b00
feelB12 <philouwebdesigner@gmail.com>
2021-07-05 17:18:27 +0200

    BDD

--- src/Controller/BlogArticleController.php
+++ src/Controller/BlogArticleController.php
@@ -1,1 +1,64 @@
 <?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+
+    private $categories = [
+        1 => [
+            "title" => "Politique",
+            "content" => "Tous les articles liés à Jean Lassalle",
+            "id" => 1,
+            "published" => true,
+        ],
+        2 => [
+            "title" => "Economie",
+            "content" => "Les meilleurs tuyaux pour avoir DU FRIC",
+            "id" => 2,
+            "published" => true
+        ],
+        3 => [
+            "title" => "Securité",
+            "content" => "Attention les étrangers sont très méchants",
+            "id" => 3,
+            "published" => false
+        ],
+        4 => [
+            "title" => "Ecologie",
+            "content" => "Hummer <3",
+            "id" => 4,
+            "published" => true
+        ]
+    ];
+
+    /**
+     * @Route("/Articles", name="articleList")
+     */
+    public function articleList()
+    {
+        return $this->render('article_list.html.twig', [
+            'articles' =>$this->articles
+        ]);
+    }
+    /**
+     * @Route("/article/{id}", name="articleShow")
+     */
+    public function articleShow($id)
+    {
+        // j'utilise la méthode render de l'AbstractController
+        // pour récupérer un fichier Twig, le transformer en HTML
+        // et le renvoyer en réponse HTTP au navigateur
+        // Pour utiliser des variables dans le fichier twig, je dois
+        // lui passer un tableau en second parametre, avec toutes les
+        // variables que je veux utiliser
+        return $this->render('article_show.html.twig', [
+            'article'=>$this->articles[$id]
+        ]);
+    }
+}
--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,1 +1,40 @@
 <?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        // je retourne une réponse valide pour ma page d'accueil
+        // je retourne  la reponse dans le HTTPFoundation
+        return new Response( 'Home Sweet Home');
+    }
+    /**
+     * @Route("/aboutus", name="About-Us")
+     */
+    public function test()
+    {
+        // je retourne une réponse valide pour ma page Qui sommes nous
+        // je retourne  la reponse dans le HTTPFoundation
+        return new Response( 'We are The Blog');
+    }
+    /**
+     * @Route("/legals", name="Legals")
+     */
+    public function legals()
+    {
+        // je retourne une réponse valide pour ma page de Mention légales
+        // je retourne  la reponse dans le HTTPFoundation
+        return new Response( 'Blog Legals' );
+    }
+}
+
